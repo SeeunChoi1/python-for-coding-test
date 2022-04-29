5377ccdae6eb06c0bf34ef79d085683d4e892c73
SeeunChoi1 <candy970106@gmail.com>
2022-04-30 03:57:46 +0900

    [fail] 5-8

---
+++ 05/8.py
@@ -0,0 +1,64 @@
+# 감시 피하기
+from re import L
+import sys
+from xml.sax.handler import feature_namespace_prefixes
+sys.stdin = open("05/8.txt", "r")
+
+n = int(input())
+graph = [ list(input().split()) for _ in range(n) ]
+blank = []
+teacher = []
+obstacle = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] == 'X':
+            blank.append([i,j])
+        if graph[i][j] == 'T':
+            teacher.append([i,j])
+dx = [0,0,-1,1] #상하좌우
+dy = [1,-1,0,0]
+
+def findStudent(x,y):
+    for i in range(4):
+        nx = x
+        ny = y
+        while True:
+            nx = nx+dx[i]
+            ny = ny+dy[i]
+            if nx<0 or nx>=n or ny<0 or ny>=n: # out of range
+                break
+            if graph[nx][ny] == 'S':
+                # print('학생 찾았다', nx,ny)
+                return True
+            elif graph[nx][ny] == 'O':
+                return False
+    return False
+
+def setObstacle(idx,tmp): #dfs permutation
+    if len(tmp) == 3:
+        obstacle.append(tmp[:])
+        return
+    for i in range(idx,len(blank)):
+        setObstacle(i+1,tmp+[blank[i]])
+
+def teacherFind():
+    for t in teacher:
+        if findStudent(t[0],t[1]):
+            return True
+    return False
+
+setObstacle(0,[])
+ans = False
+
+for obs in obstacle:
+    for x,y in obs:
+        graph[x][y] = 'O'
+    if not teacherFind():
+        ans = True
+    for x,y in obs:
+        graph[x][y] = 'X'
+
+if ans:
+    print("YES")
+else:
+    print("NO")
