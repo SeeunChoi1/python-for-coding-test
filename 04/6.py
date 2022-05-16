0586b983667d12ccab3bc8742fc3aa5b95e94193
SeeunChoi1 <candy970106@gmail.com>
2022-05-17 02:43:45 +0900

    [fix] 4-6 소스 및 주석 변경

--- 04/6.py
+++ 04/6.py
@@ -1,4 +1,5 @@
 # 자물쇠와 열쇠
+## 도저히 풀다가 GG치고 검색해봤는데 그냥 뇌절와서 완성 못한 것같기도 함
 
 # 시계방향 회전 90, 180, 270
 def rotation(key, key_size):
@@ -9,17 +10,22 @@ def rotation(key, key_size):
     return new_key
 
 # 열렸는가
-def is_fit(startX, startY, lock, key,expend_size,start,end):
+def is_fit(startX, startY, lock, key, expend_size, start, end):
     expend_lock = [[0]*(expend_size) for _ in range(expend_size)]
     
-    # expend_lock에 원래 lock 채우기
+    # expend_lock에 lock 채우기
+    for i in range(start,end):
+        for j in range(start,end):
+            expend_lock[i][j] += lock[i-start][j-start]
+    
+    # expend_lock에 key 채우기
     for i in range(len(key)):
         for j in range(len(key)):
             expend_lock[startX+i][startY+j] += key[i][j]
             
+    # 다 채웠으니 확인하기
     for i in range(start,end):
         for j in range(start,end):
-            expend_lock[i][j] += lock[i-start][j-start]
             if expend_lock[i][j] != 1:
                 return False
     return True
@@ -28,12 +34,14 @@ def solution(key, lock):
     key_size = len(key[0])
     lock_size = len(lock[0])
     
-    start = len(key)-1 # expend_lock에서 lock의 시작 지점
-    end = start + len(lock) # expend_lock에서 lock이 끝나는 지점
-    expend_size = 2*key_size+lock_size-2 # 상하좌우 이동을 위한 확장 lock
+    # expend_lock에서 lock의 원래 위치 (start,start) ~ (end,end)
+    start = len(key)-1
+    end = start + len(lock)
+    # 상하좌우 이동을 위한 확장 lock의 size
+    expend_size = 2*key_size+lock_size-2 
     
     # expend_lock 순회하면서 맞는지 확인
-    for a in range(0,4): # rotate key
+    for _ in range(0,4): # rotate key
         for i in range(end):
             for j in range(end):
                 if is_fit(i,j,lock,key,expend_size,start,end):
