597209f102a723f23d3519273ce285a0b9c26eb4
SeeunChoi1 <candy970106@gmail.com>
2022-05-24 03:41:47 +0900

    [solved] 6-6

---
+++ 06/6.py
@@ -0,0 +1,48 @@
+# 실패율
+## 정렬
+import sys
+
+
+def solution(N,stages):
+    answer = []
+
+    # 실패율 = 도달했으나 클리어 못함 / 도달한 플레이어 수
+    # [클리어 못함, 도달한 플레이어 수]
+    not_clear = [0 for _ in range(N+2)]
+    reached = [0 for _ in range(N+2)]
+    failure = [[0,0] for _ in range(N+2)]
+    stages.sort()
+    print(stages)
+
+    for stage in stages:
+        not_clear[stage] += 1 # 클리어 못한 플레이어 수 증가
+
+    reached[len(not_clear)-1] = not_clear[len(not_clear)-1]
+    for i in reversed(range(1,len(not_clear)-1)):
+        reached[i] = not_clear[i] + reached[i+1]
+
+    print(not_clear)
+    print(reached)
+
+    for i in range(1,N+2):
+        failure[i][0] = i
+        if reached[i] == 0:
+            failure[i][1] = 0
+        else:
+            failure[i][1] = not_clear[i] / reached[i]
+    print(failure)
+
+    failure.sort(key=lambda x : x[1], reverse=True)
+    print(failure)
+
+    for fail in failure:
+        if fail[0] == N+1 or fail[0] == 0:
+            continue
+        answer.append(fail[0])
+
+    return answer
+
+
+N = 4
+stages = [4,4,4,4,4]	
+print(solution(N,stages))
