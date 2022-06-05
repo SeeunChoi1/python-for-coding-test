3798ad60eef33ae8f7cd721dc107cab00b37da5c
SeeunChoi1 <candy970106@gmail.com>
2022-06-05 17:56:21 +0900

    [fix] 7-6 (iterate -> 인덱싱)

--- 07/6.py
+++ 07/6.py
@@ -1,23 +1,13 @@
 # 가사 검색
 import sys
 
-def is_equal(word,query,idx):
-    if word[idx] == query[idx]:
+def binary_search(word,query,flag,idx):
+    if flag == 'forward' and word[idx:]==query[idx:]:
+        return True
+    elif flag == 'backward' and word[:idx]==query[:idx]:
         return True
     else:
         return False
-
-def binary_search(word,query,flag,idx):
-    # print(flag,idx)
-    while (0<= idx <len(word)):
-        if is_equal(word,query,idx):
-            if flag == 'forward':
-                idx += 1
-            elif flag == 'backward':
-                idx -= 1
-        else:
-            return False
-    return True
 
 def solution(words, queries):
     answer = []
@@ -28,19 +18,20 @@ def solution(words, queries):
         flag = 'forward' if query[0]=='?' else 'backward'
         # idx : ?가 시작되는 위치
         if flag == 'backward':
-            idx = query.find('?')-1
+            idx = query.find('?')
         elif flag == 'forward':
             idx = query.rfind('?')+1
         query_len = len(query)
-        # tmp = []
+        tmp = []
         # print('keyword >> ',query,flag,idx)
 
         for word in words:
             # print('result >>',word, binary_search(word,query,flag,idx))
             if len(word)==query_len and binary_search(word,query,flag,idx):
                 match_num += 1
-                # tmp.append(word)
+                tmp.append(word)
         answer.append(match_num)
+        print(tmp)
 
     return answer
 
