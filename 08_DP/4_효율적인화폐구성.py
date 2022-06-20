6147e8ef593312b701e0cc245716636cfb96865e
SeeunChoi1 <candy970106@gmail.com>
2022-06-20 23:47:41 +0900

    [fix] 8-4 주석 추가

--- 08_DP/4_효율적인화폐구성.py
+++ 08_DP/4_효율적인화폐구성.py
@@ -1,6 +1,6 @@
 # 효율적인 화폐 구성
 import sys
-sys.stdin = open("08_DP/4-2.txt", "r")
+sys.stdin = open("08_DP/4-1.txt", "r")
 
 n_kind,m_amount = map(int, input().split())
 coins = []
@@ -14,13 +14,13 @@ dp[0] = 0
 # 불가능하면 -1
 
 # 바텀업
-# for i in range(m_amount):
-#     for coin in coins:
-#         if dp[i-coin] != 10001:
+# for i in range(m_amount): # 이 금액을
+#     for coin in coins: # 어떤 동전으로 만들 수 있나
+#         if  i-coin >= 0:
 #             dp[i] = min(dp[i-coin]+1,dp[i])
 
-for coin in coins:
-    for i in range(coin,m_amount+1):
+for coin in coins: # 이 동전으로
+    for i in range(coin,m_amount+1): # 얼마까지 만들 수 있나
         if dp[i-coin] != 10001:
             dp[i] = min(dp[i-coin]+1, dp[i])
 
